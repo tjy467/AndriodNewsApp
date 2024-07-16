03bef4dabcb9eb757286d82de4979e0c0cb2df6f
tjy467 <jerry050428@outlook.com>
2024-07-16 23:21:47 +0800

    added fragments, implements fragments switching

---
+++ app/src/main/java/com/tjy/newsapp/MineFragment.java
@@ -0,0 +1,16 @@
+package com.tjy.newsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MineFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        return inflater.inflate(R.layout.fragment_mine, container, false);
+    }
+}
--- app/src/main/java/com/tjy/newsapp/NewsActivity.java
+++
@@ -1,6 +0,0 @@
-package com.tjy.newsapp;
-
-import android.app.Activity;
-
-public class NewsActivity extends Activity {
-}
